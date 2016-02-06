532569d5246457a73ce0397c8e0baa8dfc63fcf0
Peter Brennan <peter.brennan@fbd.ie>
2016-02-06 23:52:19 +0000

    git reps
    Visual Studio

---
+++ BankTests/BankTests/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccountNS;
+
+namespace BankTests
+{
+    [TestClass]
+    public class BankAccountTests
+    {
+        // unit test code
+        [TestMethod]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            // arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // act
+            account.Debit(debitAmount);
+
+            // assert
+            //The method is rather simple.
+            //We set up a new BankAccount object with a beginning balance 
+            //and then withdraw a valid amount.We use the Microsoft unit test 
+            //framework for managed code AreEqual method to verify that the 
+            //ending balance is what we expect.
+            //A test method must meet the following requirements:
+            //The method must be decorated with the[TestMethod] attribute.
+            //The method must return void.
+            //The method cannot have parameters.
+
+          double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+    }
+}
---
+++ WebApplication5/WebApplication5/WebService1.asmx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Script.Serialization;
+
+namespace WebApplication5
+{
+    // Use "Namespace" attribute with an unique name,to make service uniquely         discoverable  
+    [WebService(Namespace = "http://tempuri.org/")]
+    // To indicate service confirms to "WsiProfiles.BasicProfile1_1" standard,   
+    // if not, it will throw compile time error.  
+    //[WebServiceBinding(ConformsTo = WsiProfiles.None)]
+    // To restrict this service from getting added as a custom tool to toolbox  
+    [System.ComponentModel.ToolboxItem(false)]
+    //As, WsiProfiles.BasicProfile1_1 doesn't support method overloading we are getting this exception.
+    //Now, either remove this “[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]” attribute or
+    //make it “[WebServiceBinding(ConformsTo = WsiProfiles.None)]”. 
+    // To allow this Web Service to be called from script, using ASP.NET AJAX  
+    [System.Web.Script.Services.ScriptService]
+    public class MyService : WebService
+    {
+        [WebMethod]
+        public int SumOfNums(int First, int Second)
+        {
+            return First + Second;
+        }
+
+
+        [WebMethod(MessageName = "SumOfFloats")]
+        public float SumOfNums(float First, float Second)
+        {
+            return First + Second;
+        }
+    }
+}
